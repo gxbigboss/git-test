db955423d72d113767c6f7aaa146f42d246a5c2d
gaoxiong <1353@qq.com>
2022-02-19 00:14:23 +0800

    second commit

--- src/main/java/com/aixuexi/git/GitTest.java
+++ src/main/java/com/aixuexi/git/GitTest.java
@@ -6,5 +6,6 @@ package com.aixuexi.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2");
     }
 }

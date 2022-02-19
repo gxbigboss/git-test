3e876e11c3926e8041f17a56820391c5296987fb
gaoxiong <1353@qq.com>
2022-02-19 10:53:27 +0800

    gitee commit

--- src/main/java/com/aixuexi/git/GitTest.java
+++ src/main/java/com/aixuexi/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hof-fix test!!");
         System.out.println("push test!!");
         System.out.println("pull test!!");
+        System.out.println("gitee test!!!!");
     }
 }
